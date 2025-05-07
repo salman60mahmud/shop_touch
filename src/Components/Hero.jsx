e2e80c62645c4d7e55fb12b229522355a4b2cf5f
unknown <salman60mahmud>
2025-05-07 20:56:24 +0600

    carousel hidden

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -43,24 +43,24 @@ const Hero = () => {
                         className={`absolute inset-0 transition-opacity duration-500 flex flex-col md:flex-row items-center ${index === currentIndex ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}
                     >
                         {/* Text Content */}
-                        <div className="w-full md:w-1/2 p-8 md:pl-12 lg:pl-20 flex flex-col justify-center gap-4 sm:gap-6 md:gap-8 lg:gap-10 text-center md:text-left lg:ml-16">
-                            <h2 className="text-3xl sm:text-4xl md:text-4xl xl:text-5xl font-semibold text-neutral-900">NEW ARRIVALS ONLY</h2>
+                        <div className="w-full md:w-1/2 p-8 md:pl-12 lg:pl-20 flex flex-col justify-center gap-4 sm:gap-6 md:gap-8 lg:gap-10 text-center md:text-left lg:ml-16 my-auto md:my-0">
+                            <h2 className="text-xl sm:text-4xl md:text-4xl xl:text-5xl font-semibold text-neutral-900">NEW ARRIVALS ONLY</h2>
                             <div>
                                 <div className="flex items-center justify-center md:justify-start gap-2 sm:gap-4">
-                                    <p className="text-4xl sm:text-5xl lg:text-6xl xl:text-7xl font-medium">New</p>
+                                    <p className="text-4xl sm:text-5xl lg:text-6xl xl:text-7xl font-semibold">New</p>
                                     <img className="h-16 sm:h-20 md:h-24 w-auto" src="/src/media/hand_icon.png" alt="hand-icon" />
                                 </div>
                                 <p className="text-4xl sm:text-5xl lg:text-6xl xl:text-7xl font-semibold mt-2 sm:mt-4">Collections</p>
                                 <p className="text-4xl sm:text-5xl lg:text-6xl xl:text-7xl font-semibold mt-2 sm:mt-4 md:mt-6">For Everyone</p>
                             </div>
-                          <button className="btn btn-secondary hover:bg-secondary-content hover:text-secondary h-12 sm:h-14 md:h-16 xl:w-[400px] md:w-[360px] sm:w-auto  mx-auto md:mx-0 px-6 rounded-full flex items-center justify-center gap-2 sm:gap-4">
+                            <button className="btn btn-secondary hover:bg-secondary-content hover:text-secondary h-12 sm:h-14 md:h-16 xl:w-[400px] md:w-[360px] sm:w-auto  mx-auto md:mx-0 px-6 rounded-full flex items-center justify-center gap-2 sm:gap-4">
                                 <span className="text-xl sm:text-2xl md:text-3xl font-medium">Latest Collection</span>
                                 <FaArrowRight className="text-xl sm:text-2xl" />
                             </button>
                         </div>
 
                         {/* Image */}
-                        <div className="w-full md:w-1/2 p-4 md:p-8 lg:p-12 flex justify-center">
+                        <div className="w-full md:w-1/2 p-4 md:p-8 lg:p-12 md:flex justify-center hidden">
                             <img
                                 src={slide.image}
                                 className="w-full max-w-md md:max-w-lg lg:max-w-xl h-auto object-contain"
@@ -72,13 +72,13 @@ const Hero = () => {
             </div>
 
             {/* Indicators */}
-            <div className="absolute bottom-4 left-0 right-0 flex justify-center space-x-2 z-10">
+            <div className="absolute bottom-4 left-0 right-0 md:flex justify-center space-x-2 z-10 hidden">
                 {slides.map((_, index) => (
                     <button
                         key={index}
                         type="button"
                         onClick={() => showSlide(index)}
-                        className={`w-2 h-2 sm:w-3 sm:h-3 rounded-full bg-white ${index === currentIndex ? 'opacity-100' : 'opacity-50'} hover:opacity-100 transition-opacity duration-300`}
+                        className={`w-2 h-2 sm:w-3 sm:h-3 rounded-full bg-secondary ${index === currentIndex ? 'bg-white' : 'opacity-100'} hover:bg-white transition-opacity duration-300`}
                         aria-label={`Slide ${index + 1}`}
                     ></button>
                 ))}
@@ -86,7 +86,7 @@ const Hero = () => {
 
             {/* Controls */}
             <button
-                className="absolute top-1/2 left-2 sm:left-4 md:left-6 transform -translate-y-1/2 bg-secondary hover:bg-secondary-content hover:text-secondary p-1 sm:p-2 md:p-3 rounded-full text-white transition-colors duration-300 z-10"
+                className="absolute top-1/2 left-2 sm:left-4 md:left-6 transform -translate-y-1/2 bg-secondary hover:bg-secondary-content hover:text-secondary p-1 sm:p-2 md:p-3 rounded-full text-white transition-colors duration-300 z-10 hidden md:block"
                 type="button"
                 onClick={prevSlide}
             >
@@ -94,7 +94,7 @@ const Hero = () => {
                 <span className="sr-only">Previous</span>
             </button>
             <button
-                className="absolute top-1/2 right-2 sm:right-4 md:right-6 transform -translate-y-1/2 bg-secondary hover:bg-secondary-content hover:text-secondary p-1 sm:p-2 md:p-3 rounded-full text-white transition-colors duration-300 z-10"
+                className="absolute top-1/2 right-2 sm:right-4 md:right-6 transform -translate-y-1/2 bg-secondary hover:bg-secondary-content hover:text-secondary p-1 sm:p-2 md:p-3 rounded-full text-white transition-colors duration-300 z-10 hidden md:block"
                 type="button"
                 onClick={nextSlide}
             >
